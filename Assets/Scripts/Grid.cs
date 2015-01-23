671608bc29e90b828d980c175102fadc7202c943
Matthew Dyer <matthew.dyer@aura.org.nz>
2015-01-24 10:38:51 +1300

    things!

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public  int width;
+	public  int height;
+	public  int gridSize;
+
+	private Placeable[,] grid;
+	// Use this for initialization
+	void Start () {
+		grid = new Placeable[width,height];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void placeObject(Placeable p,int xPixel,int yPixel){
+		//Horizontal
+		int xSquare = xPixel / gridSize;
+		int ySquare = yPixel / gridSize;
+
+		if (!(grid[xSquare, ySquare] == null)){
+			grid[xSquare,ySquare] = p;
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Placeable.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Placeable : MonoBehaviour {
+
+	public GameObject foo;
+
+	public double xPos;
+	public double yPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+
+	
+}
