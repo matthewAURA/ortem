9bdb11db882cc7be36f005192fa61a16dd0f49fe
Glen <phoglenix@gmail.com>
2015-01-24 22:44:24 +1300

    warnings

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class cameraController : MonoBehaviour {
 
-	public Transform camera;
+	public Transform cameraTrans;
 	public float scrollSpeed = 0.2f;
 	public float edgeScrollPercent = 0.01f;
 	// Use this for initialization
@@ -14,30 +14,30 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.mousePosition.x < Screen.width * edgeScrollPercent) {
-			camera.Translate(new Vector3(-scrollSpeed,0f));
+			cameraTrans.Translate(new Vector3(-scrollSpeed,0f));
 		}
 		if (Input.mousePosition.x > Screen.width * 1-(edgeScrollPercent)) {
-			camera.Translate(new Vector3(scrollSpeed,0f));
+			cameraTrans.Translate(new Vector3(scrollSpeed,0f));
 		}
 		if (Input.mousePosition.y < Screen.height * edgeScrollPercent) {
-			camera.Translate(new Vector3(0f,-scrollSpeed));
+			cameraTrans.Translate(new Vector3(0f,-scrollSpeed));
 		}
 		if (Input.mousePosition.y > Screen.height *(1-edgeScrollPercent)) {
-			camera.Translate(new Vector3(0f,scrollSpeed));
+			cameraTrans.Translate(new Vector3(0f,scrollSpeed));
 		}
 
 
 		if (Input.GetKey ("w")) {
-			camera.Translate(new Vector3(0,scrollSpeed));
+			cameraTrans.Translate(new Vector3(0,scrollSpeed));
 		}
 		if (Input.GetKey ("s")) {
-			camera.Translate(new Vector3(0,-scrollSpeed));
+			cameraTrans.Translate(new Vector3(0,-scrollSpeed));
 		}
 		if (Input.GetKey ("a")) {
-			camera.Translate(new Vector3(-scrollSpeed,0));
+			cameraTrans.Translate(new Vector3(-scrollSpeed,0));
 		}
 		if (Input.GetKey ("d")) {
-			camera.Translate(new Vector3(scrollSpeed,0));
+			cameraTrans.Translate(new Vector3(scrollSpeed,0));
 		}
 	}
 }
